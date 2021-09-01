15ccd5769d370e9099841a4b0a85d64256dff4f6
Stella <stella.taylor@clicktravel.com>
2021-09-01 15:45:36 +0100

    updated admin to support custome user model

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -15,15 +15,29 @@ class AdminSiteTests(TestCase):
 # Means we don't have to manually log users in
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='admin@hotmail.com',
+            email='test@hotmail.com',
             password='test1234',
             name='Test user full name'
         )
 
     def test_users_listed(self):
         """Test that users are listed on user page"""
-        url = reverse('admin:core_user_changeList')
+        url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
